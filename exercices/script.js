5aad6a7af6a0c005ac3bc9292901f07dcbe17b32
Eden <145431210+Eden2011@users.noreply.github.com>
2023-09-22 12:26:47 +0200

    Add files via upload

---
+++ exercices/script.js
@@ -0,0 +1,26 @@
+"use strict";
+
+let avengers = 14; //number
+let Jules = "César"; //string
+let univers = "10 puissance 23"; //number
+let bienvenue = "welcome"; //string
+let faceID = true; //boolean
+let prixOrdi = "cher"; //string, si c'était un chiffre > number
+let nains = "Prof, SImplet, Dormeur, Grincheux, Atchoum, Joyeux et Timide"; //tableau type objet
+let bgTinder = "Coco du BUT"; //string
+
+console.log(avengers + " "+ Jules + " "+ univers + " "+ bienvenue+ " "+ faceID+ " "+ prixOrdi+" "+ nains+ " "+bgTinder+ ".");
+
+let clonesNaruto= "bcp"; //string
+let défauts = "émotif, obstiné, méfiant"; //tableau type objet
+let Anne = "Anne"; //string
+let panier = "panier"; //string
+let centAns = 100; //number
+let bière = "levure"; //string
+let président = "Zemmour"; //string
+
+console.log(clonesNaruto+ " "+ défauts+ " "+ Anne+ " "+ panier+ " "+ centAns+ " "+ bière+ "  "+ président+ ".");
+
+const humourLu = "nul"; //une fois déclarée, pas modifiable
+humourLu = "pas ouf";
+console.log(humourLu);
